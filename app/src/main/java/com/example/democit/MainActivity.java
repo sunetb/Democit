3f299e7efa923d0bf45caa9e989dcb21439b38bd
sunetb <sunetb@gmail.com>
2023-03-06 12:53:56 +0100

    ava.net.SocketException: socket failed: EPERM (Operation not permitted)

--- app/src/main/java/com/example/democit/MainActivity.java
+++ app/src/main/java/com/example/democit/MainActivity.java
@@ -8,6 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView hello;
@@ -35,12 +44,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        data.myName = "Good morning "+ input.getText().toString();
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                data.myName = readURL("https://raw.githubusercontent.com/sunetb/U/master/greeting.txt");//"Good morning "+ input.getText().toString();
+
+            }
+        });
+        t.start();
         hello.setText(data.myName);
+
 
 
         System.out.println(input.getText().toString());
 
 
     }
+
+    String readURL(String inputUrl){
+        String value = "";
+        URL url = null;
+        try {
+            url = new URL(inputUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            System.out.println(con);
+            con.setRequestMethod("GET");
+            InputStream i = con.getInputStream();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(i));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+                value += inputLine + "\n";
+            }
+            in.close();
+        } catch (ProtocolException ex) {
+            throw new RuntimeException(ex);
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return value;
+    }
+
 }
